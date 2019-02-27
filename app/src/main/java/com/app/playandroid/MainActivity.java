45d65d296af86de77a9d3df8f6359441a0f2deee
Qiaop <m13681477724@163.com>
2019-02-27 20:58:57 +0800

    iiiiii

--- app/src/main/java/com/app/playandroid/MainActivity.java
+++ app/src/main/java/com/app/playandroid/MainActivity.java
@@ -3,11 +3,18 @@ package com.app.playandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+    }
+
+    private void initData() {
+        ArrayList<Object> objects = new ArrayList<>();
     }
 }
